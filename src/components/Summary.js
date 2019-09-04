c26d7aa9721623c627d908b798bae73d0296c15b
christianshehata <h1607903@s.wu.ac.at>
2019-09-04 18:23:39 +0200

    Minor CSS Update

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -10,20 +10,19 @@ class Summary extends React.Component {
             <div className="summary">
                 <h4> Hi there, this is your configuration</h4>
                 <h6> Table Size </h6>
-                <h6> {this.props.chosenTable} {this.props.priceOfTable} € </h6>
+                <h5>  - {this.props.chosenTable} </h5>
                 <h6> Monitor Size </h6>
-                <h6> {this.props.chosenMonitorSize} </h6>
+                <h5>  - {this.props.chosenMonitorSize} </h5>
                 <h6> Server Size </h6>
-                <h6> {this.props.chosenServerSize} </h6>
+                <h5>  - {this.props.chosenServerSize} </h5>
                 <h6> Adjust height? </h6>
-                <h6> {this.props.chosenHeight} </h6>
+                <h5>  - {this.props.chosenHeight} </h5>
                 <h6> Need Light? </h6>
-                <h6> {this.props.chosenLight} </h6>
+                <h5>  - {this.props.chosenLight} </h5>
                 <h6> Powerboards? </h6>
-                <h6> {this.props.chosenPowerboard} </h6>
+                <h5>  - {this.props.chosenPowerboard} </h5>
                 <h6> Preferred Assembler? </h6>
-                <h6> {this.props.chosenAssembler} </h6>
-                <h6> Total Account </h6>
+                <h5>  - {this.props.chosenAssembler} </h5>
             </div>
         )
     }
