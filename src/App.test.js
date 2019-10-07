6df1ba89936b4b13a45cee9716b0d8bf7540de93
christianshehata <h1607903@s.wu.ac.at>
2019-10-07 23:47:33 +0200

    .lfsconfig

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import table from "./assets/table_white.png";
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+
+it('return images', () => {
+  const small_table = <img src={table} height="500" width="600" alt="ref"/>
+
+})
