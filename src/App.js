eaef3eb2b013b9e724fefca3dd088b9a5cd1cba7
Christian Shehata <h1607903@s.wu.ac.at>
2020-01-01 14:40:26 +0100

    ErrorBoundary.js Update

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class App extends Component{
           />
           <Intro />
           <ErrorBoundary>
-             <Settings />
+            <Settings />
           </ErrorBoundary>
       </div>
   );
